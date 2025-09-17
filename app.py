2d2b1bf7f42f118cda1b9482a9ee92924c34250a
Rosh264 <165977734+Rosh264@users.noreply.github.com>
2025-09-17 07:56:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import torchaudio
 import os
 import io
 import numpy as np
-from streamlit_audiorec import st_audiorec
+from st_audiorec import st_audiorec
 import matplotlib.pyplot as plt
 
 # --- Step 1: Define Model Architecture & Load Expert Model ---
